8f357e237c6f45df68f1c0fbc945088a61dc4a28
Ca-tt <damir96.lukyanenko@gmail.com>
2025-05-24 11:56:36 +0300

    improve RU texts

--- src/config/env.py
+++ src/config/env.py
@@ -11,4 +11,3 @@ DATABASE_TOKEN: str = dotenv.get("DATABASE_TOKEN") or ""
 
 SUPER_ADMIN_ID: int = dotenv.get_int("SUPER_ADMIN_ID")
 ADMIN_IDS: list[int] = dotenv.get_list_of_ints("ADMIN_IDS")
-# USER_IDS: list[int] = dotenv.get_list_of_ints("USER_IDS")
